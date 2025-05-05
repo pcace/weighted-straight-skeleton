efe66e3d0a0861fecd305c2ad1ec6400a67cb385
Johannes Frölich <mail@johannes-froelich.de>
2025-05-05 16:16:58 +0200

    fix: :bug: revert

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,58 @@
 const path = require("node:path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
 
 module.exports = [
-    {
-        entry: "./src/wrapper/index.ts",
-        output: {
-            filename: "./index.js",
-            path: path.resolve(__dirname, "dist"),
-            library: {
-                type: "umd",
-            },
-        },
-        // plugins: [new CleanWebpackPlugin()],
-        devtool: "source-map",
-        module: {
-            rules: [
-                {
-                    test: /\.ts|.tsx$/,
-                    loader: "ts-loader",
-                    exclude: /node_modules/,
-                    options: { configFile: "tsconfig.build.json" },
-                },
-            ],
-        },
-        resolve: {
-            extensions: [".ts", ".js"],
-        },
-    },
-    {
-        // Node.js build
-        entry: "./src/wrapper/index.ts",
-        output: {
-            filename: "./index.node.js",
-            path: path.resolve(__dirname, "dist"),
-            library: {
-                type: "commonjs2",
-            },
-            globalObject: "globalThis", // Use globalThis instead of 'self'
-        },
-        target: "node",
-        devtool: "source-map",
-        module: {
-            rules: [
-                {
-                    test: /\.ts|.tsx$/,
-                    loader: "ts-loader",
-                    exclude: /node_modules/,
-                    options: { configFile: "tsconfig.build.json" },
-                },
-            ],
-        },
-        resolve: {
-            extensions: [".ts", ".js"],
-        },
-        plugins: [
-            // Provide global variables for Node environment
-            new webpack.DefinePlugin({
-                self: "global", // Define 'self' as 'global' for Node.js
-            }),
-        ],
-    },
+
+	{
+		entry: "./src/wrapper/index.ts",
+		output: {
+			filename: "./index.js",
+			path: path.resolve(__dirname, "dist"),
+			library: {
+				type: "umd",
+			},
+		},
+		// plugins: [new CleanWebpackPlugin()],
+		devtool: "source-map",
+		module: {
+			rules: [
+				{
+					test: /\.ts|.tsx$/,
+					loader: "ts-loader",
+					exclude: /node_modules/,
+					options: { configFile: "tsconfig.build.json" },
+				},
+			],
+		},
+		resolve: {
+			extensions: [".ts", ".js"],
+		},
+	},
+	{
+		// Node.js build
+		entry: "./src/wrapper/index.ts",
+		output: {
+			filename: "./index.node.js",
+			path: path.resolve(__dirname, "dist"),
+			library: {
+				type: "commonjs2",
+			},
+		},
+		target: "node",
+		devtool: "source-map",
+		module: {
+			rules: [
+				{
+					test: /\.ts|.tsx$/,
+					loader: "ts-loader",
+					exclude: /node_modules/,
+					options: { configFile: "tsconfig.build.json" },
+				},
+			],
+		},
+		resolve: {
+			extensions: [".ts", ".js"],
+		},
+	},
 ];
