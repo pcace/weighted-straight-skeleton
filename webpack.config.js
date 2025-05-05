e37bfbaa299e4da984d52e9e7b621854247341ac
Johannes Frölich <mail@johannes-froelich.de>
2025-05-05 16:55:54 +0200

    fix: :bug: revert to web build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
-const path = require("node:path");
+const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = [
-
 	{
 		entry: "./src/wrapper/index.ts",
 		output: {
@@ -13,45 +11,8 @@ module.exports = [
 				type: "umd",
 			},
 		},
-		// plugins: [new CleanWebpackPlugin()],
 		devtool: "source-map",
-		module: {
-			rules: [
-				{
-					test: /\.ts|.tsx$/,
-					loader: "ts-loader",
-					exclude: /node_modules/,
-					options: { configFile: "tsconfig.build.json" },
-				},
-			],
-		},
-		resolve: {
-			extensions: [".ts", ".js"],
-            fallback: {
-                // Just use empty modules instead of polyfills
-                "path": false,
-                "fs": false,
-                "crypto": false,
-                "stream": false,
-                "buffer": false,
-                "util": false,
-            },
-
-
-		},
-	},
-	{
-		// Node.js build
-		entry: "./src/wrapper/index.ts",
-		output: {
-			filename: "./index.node.js",
-			path: path.resolve(__dirname, "dist"),
-			library: {
-				type: "commonjs2",
-			},
-		},
-		target: "node",
-		devtool: "source-map",
+		plugins: [new CleanWebpackPlugin()],
 		module: {
 			rules: [
 				{
