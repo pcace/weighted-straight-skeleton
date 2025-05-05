30cad91d5f50c55e2e41dfe23f4977198646bf1d
Johannes Frölich <mail@johannes-froelich.de>
2025-05-05 16:08:32 +0200

    fix: :bug: build for commonjs too

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,86 @@
-const path = require('path');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("node:path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = [{
-	entry: './src/example/index.ts',
-	output: {
-		filename: './main.js',
-		path: path.resolve(__dirname, 'example')
+module.exports = [
+	// {
+	// 	entry: "./src/example/index.ts",
+	// 	output: {
+	// 		filename: "./main.js",
+	// 		path: path.resolve(__dirname, "example"),
+	// 	},
+	// 	devtool: "inline-source-map",
+	// 	plugins: [
+	// 		new CleanWebpackPlugin(),
+	// 		new HtmlWebpackPlugin({
+	// 			filename: "index.html",
+	// 			template: "./src/example/index.html",
+	// 			minify: false,
+	// 		}),
+	// 	],
+	// 	module: {
+	// 		rules: [
+	// 			{
+	// 				test: /\.ts|.tsx$/,
+	// 				loader: "ts-loader",
+	// 				exclude: /node_modules/,
+	// 				options: { configFile: "tsconfig.dev.json" },
+	// 			},
+	// 		],
+	// 	},
+	// 	resolve: {
+	// 		extensions: [".ts", ".js"],
+	// 	},
+	// },
+	{
+		entry: "./src/wrapper/index.ts",
+		output: {
+			filename: "./index.js",
+			path: path.resolve(__dirname, "dist"),
+			library: {
+				type: "umd",
+			},
+		},
+		plugins: [new CleanWebpackPlugin()],
+		devtool: "source-map",
+		module: {
+			rules: [
+				{
+					test: /\.ts|.tsx$/,
+					loader: "ts-loader",
+					exclude: /node_modules/,
+					options: { configFile: "tsconfig.build.json" },
+				},
+			],
+		},
+		resolve: {
+			extensions: [".ts", ".js"],
+		},
 	},
-	devtool: 'inline-source-map',
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: './src/example/index.html',
-			minify: false
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.ts|.tsx$/,
-				loader: 'ts-loader',
-				exclude: /node_modules/,
-				options: {configFile: 'tsconfig.dev.json'}
-			}
-		]
+	{
+		// Node.js build
+		entry: "./src/wrapper/index.ts",
+		output: {
+			filename: "./index.node.js",
+			path: path.resolve(__dirname, "dist"),
+			library: {
+				type: "commonjs2",
+			},
+		},
+		target: "node",
+		devtool: "source-map",
+		module: {
+			rules: [
+				{
+					test: /\.ts|.tsx$/,
+					loader: "ts-loader",
+					exclude: /node_modules/,
+					options: { configFile: "tsconfig.build.json" },
+				},
+			],
+		},
+		resolve: {
+			extensions: [".ts", ".js"],
+		},
 	},
-	resolve: {
-		extensions: ['.ts', '.js']
-	},
-}, {
-	entry: './src/wrapper/index.ts',
-	output: {
-		filename: './index.js',
-		path: path.resolve(__dirname, 'dist'),
-		library: {
-			type: 'umd',
-		}
-	},
-	devtool: 'source-map',
-	plugins: [
-		new CleanWebpackPlugin()
-	],
-	module: {
-		rules: [
-			{
-				test: /\.ts|.tsx$/,
-				loader: 'ts-loader',
-				exclude: /node_modules/,
-				options: {configFile: 'tsconfig.build.json'}
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.ts', '.js']
-	},
-}];
+];
