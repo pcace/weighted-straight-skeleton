b079c80e096d6fd786ef1d08235b8f98c9372185
Johannes Frölich <mail@johannes-froelich.de>
2025-05-05 17:20:28 +0200

    fix: :bug: build for web + node

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const path = require("path");
+const path = require("node:path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = [
 	{
@@ -12,7 +13,33 @@ module.exports = [
 			},
 		},
 		devtool: "source-map",
-		plugins: [new CleanWebpackPlugin()],
+		plugins: [new CleanWebpackPlugin(), new NodePolyfillPlugin()],
+		module: {
+			rules: [
+				{
+					test: /\.ts|.tsx$/,
+					loader: "ts-loader",
+					exclude: /node_modules/,
+					options: { configFile: "tsconfig.build.json" },
+				},
+			],
+		},
+		resolve: {
+			extensions: [".ts", ".js"],
+		},
+	},
+	{
+		// Node.js build
+		entry: "./src/wrapper/index.ts",
+		output: {
+			filename: "./index.node.js",
+			path: path.resolve(__dirname, "dist"),
+			library: {
+				type: "commonjs2",
+			},
+		},
+		target: "node",
+		devtool: "source-map",
 		module: {
 			rules: [
 				{
